1c045d1e7ba7ccf78ba608f9b654c19ba80ffc87
HamzaCHEKLAT <39276108+hamzacheklat@users.noreply.github.com>
2026-01-28 11:22:42 +0100

    Create Import.ts

---
+++ app/Import.ts
@@ -0,0 +1,12 @@
+imports: [
+  FormsModule,
+  HttpClientModule,
+  MarkdownModule.forRoot(),
+
+  MatDialogModule,
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatProgressSpinnerModule
+]
